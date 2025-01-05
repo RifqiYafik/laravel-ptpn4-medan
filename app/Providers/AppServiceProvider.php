abc8a63150a69a687708ee4f6b891553d2b60498
rifqiyafika50 <rifqiyafika50@gmail.com>
2025-01-05 04:52:48 +0000

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+        \URL::forceScheme('https');
+    }
         Paginator::useBootstrap();
     }
 }
